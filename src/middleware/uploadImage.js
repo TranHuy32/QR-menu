a302f8f420f7d88ef989fcbe4c7f19f125b7f54f
tiepxuanhoangg <hoangxuantiep2001@gmail.com>
2024-07-06 00:39:49 +0700

    update code:create randomName when upload image

--- src/middleware/uploadImage.js
+++ src/middleware/uploadImage.js
@@ -1,14 +1,12 @@
 import multer from "multer"
-import random_name from 'node-random-name';
-
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, "./uploads")
     },
     filename: function (req, file, cb) {
-      const uniqueSuffix = random_name(Math.random() * 1E9);
-      cb(null,uniqueSuffix +''+ file.originalname);
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, uniqueSuffix + '-' + file.originalname )
   }
   })
   const upload = multer({ storage: storage })
