7f081033303cc3c533a456ef9478bdce9a62e2c0
Tran Huy <tranxuanhuy3201@gmail.com>
2024-08-05 00:24:04 +0700

    fix cors

--- src/loaders/expressLoader.js
+++ src/loaders/expressLoader.js
@@ -6,6 +6,7 @@ const api = require('../api/routes');
 
 module.exports = (app) => {
   // define any middlewares that need to run befoure our routes
+  app.use(cors());
 
   app.use(express.json());
   app.use(
@@ -17,9 +18,4 @@ module.exports = (app) => {
   app.use(api);
 
   // Use CORS middleware
-  app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-    allowedHeaders: ['Content-Type', 'Authorization'] 
-  }));
 };
