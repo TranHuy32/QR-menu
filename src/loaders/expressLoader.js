d42b9c8b467deb261282b7614e8abe8583143ee9
huy132 <nguyenquanghuymt1@gmail.com>
2024-08-05 01:49:25 +0700

    searchDishByCategory

--- src/loaders/expressLoader.js
+++ src/loaders/expressLoader.js
@@ -7,6 +7,7 @@ const api = require('../api/routes');
 module.exports = (app) => {
   // define any middlewares that need to run befoure our routes
   app.use(cors());
+
   app.use(express.json());
   app.use(
     express.urlencoded({
@@ -16,4 +17,5 @@ module.exports = (app) => {
   // define ALL routes here
   app.use(api);
 
+  // Use CORS middleware
 };
