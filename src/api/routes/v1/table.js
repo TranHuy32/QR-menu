5f875de0fbe679b665a856664b741c657a682b78
huy132 <nguyenquanghuymt1@gmail.com>
2024-08-29 00:05:42 +0700

    deleteAndU[dateOption

--- src/api/routes/v1/table.js
+++ src/api/routes/v1/table.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { createTable, getTables, getTablesById,updatedTablesById,deleteTablesById,activeByUuid, getTablesByUuid } from "../../../services/tableServices.js";
-import {verifyToken} from "../../../middleware/auth.js";
+import { createTable, getTables, getTablesById, updatedTablesById, deleteTablesById, activeByUuid, getTablesByUuid } from "../../../services/tableServices.js";
+import { verifyToken } from "../../../middleware/auth.js";
 
 const route = Router();
 
 const tableRoutes = (app) => {
   app.use("/table", route);
-// search all table
-  route.get("/all",verifyToken, async (req, res, next) => {
+  // search all table
+  route.get("/all", verifyToken, async (req, res, next) => {
     try {
       const tables = await getTables(req);
       res.status(200).json({ status: 200, listTable: tables });
@@ -18,7 +18,7 @@ const tableRoutes = (app) => {
     }
   });
   // search table by ID
-  route.get("/:id",verifyToken, async (req, res, next) => {
+  route.get("/:id", verifyToken, async (req, res, next) => {
     try {
       const tableId = await getTablesById(req);
       res.status(200).json({ status: 201, listTable: tableId });
@@ -29,18 +29,18 @@ const tableRoutes = (app) => {
     }
   });
   // search table by uuid active
-  route.get("/",verifyToken, async (req, res, next) => {
+  route.get("/", verifyToken, async (req, res, next) => {
     try {
-        console.log('Request received at GET /:uuid');
-        const tableUuid = await getTablesByUuid(req, res); // Truyền cả req và res
-        res.status(200).json({ status: 201, listTable: tableUuid });
+      console.log('Request received at GET /:uuid');
+      const tableUuid = await getTablesByUuid(req, res); // Truyền cả req và res
+      res.status(200).json({ status: 201, listTable: tableUuid });
     } catch (error) {
-        console.error('Error in GET /:uuid:', error);
-        res.status(500).json({ status: 500, message: "Invalid table UUID" });
+      console.error('Error in GET /:uuid:', error);
+      res.status(500).json({ status: 500, message: "Invalid table UUID" });
     }
-});
+  });
 
-  route.patch("/",verifyToken, async (req, res, next) => {
+  route.patch("/", verifyToken, async (req, res, next) => {
     try {
       console.log(111111);
       const tableId = await activeByUuid(req);
@@ -52,9 +52,9 @@ const tableRoutes = (app) => {
     }
   });
   // sua table theo ID
-  route.put("/:id",verifyToken, async (req, res, next) => {
+  route.put("/:id", verifyToken, async (req, res, next) => {
     try {
-      const updatedTableId = await updatedTablesById (req);
+      const updatedTableId = await updatedTablesById(req);
       res.status(200).json({ status: 202, listTable: updatedTableId.message });
     } catch (error) {
       //   return next(err);
@@ -63,9 +63,9 @@ const tableRoutes = (app) => {
     }
   });
   // delete table by ID
-  route.delete("/:id",verifyToken, async (req, res, next) => {
+  route.delete("/:id", verifyToken, async (req, res, next) => {
     try {
-      const deleteTableId = await deleteTablesById (req);
+      const deleteTableId = await deleteTablesById(req);
       res.status(204).json({ status: 202, listTable: deleteTableId.message });
     } catch (error) {
       //   return next(err);
@@ -74,7 +74,7 @@ const tableRoutes = (app) => {
     }
   });
 
-  route.post("/",verifyToken, async (req, res, next) => {
+  route.post("/", verifyToken, async (req, res, next) => {
     try {
       const table = await createTable(req);
       res.status(200).json({ status: 200, newTable: table });
